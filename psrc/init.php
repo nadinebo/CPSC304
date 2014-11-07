bf4f1a1a14db79d6d19aaafeaf70d75e88ded544
wontonsolutions <stewbertgrant@gmail.com>
2014-11-07 09:01:09 -0800

    addition of testing header to houns init

--- psrc/init.php
+++ psrc/init.php
@@ -6,13 +6,15 @@
 
 <body>
 	<?php
+		echo "Welcome to Houns ";
 		$server = '127.0.0.1';
 		$user = 'root';
-		$pass = '';
+		$pass = 'iwicbV15';
 		$dbname = 'Houns';
 
 		$connection = new mysqli($server, $user, $pass, $dbname);
-		//mysql_select_db($dbname);
+		mysql_select_db($dbname);
+		echo "Connection attempt made";
 	    
 	    if (!mysqli_connect_errno()) {
 	        echo "You connected!";
