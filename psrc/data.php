9ad46b98703c0a76358926d46fb14dbb23cb71af
wontonsolutions <stewbertgrant@gmail.com>
2014-11-10 14:55:48 -0800

    addition of the deletion and select all functions for leadSinger class. Reference calls left in init for everyone else to take a look at

--- psrc/data.php
+++ psrc/data.php
@@ -41,4 +41,34 @@ function insertLeadSinger($UPC,$Name)
 		echo "<b>Successfully added ".$Name."</b>";
 	}
 }
+
+function selectLeadSinger()
+{
+	error_reporting(E_STRICT);
+	echo "   query lead singer   ";
+	global $connection;
+	if(!$result = $connection->query("Select * From LeadSinger")) {
+		die('There was an error running the query [' .$db->error . ']');
+	} else {
+		echo "<b>Search succussfull<\b>";
+	}
+	return $result;
+
+}
+
+function deleteLeadSinger($UPC,$Name)
+{
+	error_reporting(E_STRICT);
+	echo "  deleting lead singer   ";
+	global $connection;
+	$stmt = $connection->prepare("DELETE FROM LeadSinger WHERE upc=? AND name=?");
+	$stmt->bind_param("is",$UPC,$Name);
+	$stmt->execute();
+	if($stmt->error) {
+		printf("<b>Error: %s. </b>\n", $stmt->error);
+	} else {
+		echo "<b>Successfully deleted ".$Name."</b>";
+	}
+}
+
 ?>
--- psrc/init.php
+++ psrc/init.php
@@ -14,8 +14,18 @@
 		initLogic();
 		initPresentation();
 		echo "initComplete";
+		
+		//Reference code for everyone else while making the getting and setting classes
 		insertLeadSinger('384932647092','St.Vincent');
+		$result = selectLeadSinger();
+		//a bit of test display for the sake of it
+		while($row = $result->fetch_assoc()){
+			echo"<td>".$row['upc']."</td>";
+			echo"<td>".$row['name']."</td>";
+		}
+		deleteLeadSinger('384932647092','St.Vincent');
 		echo "insert comp";
+		//end reference
 	?>
 </body>
 </html>
