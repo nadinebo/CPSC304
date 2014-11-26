630eb67c4bf24b88c6692302f7e4c1190a690a57
wontonsolutions <stewbertgrant@gmail.com>
2014-11-26 12:42:13 -0800

    basket buggy but basic functions work, bugs 1) after refresh another add occurs. 2) after add, a refresh is needed to see the upadate on the shopping basket, 3) adding with too many items greater then stock does not submit

--- pages/customerHome.php
+++ pages/customerHome.php
@@ -74,153 +74,190 @@ $P = new Presentation();
 ?>
 				<div role="tabpanel" class="tab-pane" id="cart">
 					<h3> The Shopping Cart </h3>
-				<?php
-				session_start();
-				if(!isset($_SESSION['shoppingBasket'])){
-					//$_SESSION['shoppingBasket'] = null;
-					//echo "new basket";
-				}
-				$basket = $_SESSION['shoppingBasket'];
-			/*	
-				$item1['upc'] = "23";
-				$item1['stock'] = "45";
+<?php
+session_start();
+if(!isset($_SESSION['shoppingBasket'])){
+	//$_SESSION['shoppingBasket'] = null;
+	//echo "new basket";
+}
+$basket = $_SESSION['shoppingBasket'];
 
-				$item2['upc'] = "66";
-				$item2['title'] = "wham";
-				$item2['year'] = "4589";
-				
-				$item3['upc'] = "55";
-				$item3['title'] = "do";
-				$item3['year'] = "mosi";
-				
-				$basket[0] = $item1;
-				$basket[1] = $item2;
-				$basket[2] = $item3;
-				//unset($basket[0]);
-			 */
-				if($_SERVER["REQUEST_METHOD"] == "POST") {
-					echo "<h1>POST</h1>";
-					if(isset($_POST["submitDelete"]) && $_POST["submitDelete"] == "DELETE"){
-						echo "<h1>ITEM DELETED</h1>";
-						$deleteUPC = $_POST['upc'];
-						echo $deleteUPC;
-						for($i=0;$i<count($basket);$i++){
-							$item = $basket[$i];
-							if($item['upc'] == $deleteUPC){
-								$basket[$i]=null;
-								break;
-							}
-						}
-					}
-				}
-				$_SESSION['shoppingBasket']=$basket;
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+	echo "<h1>POST</h1>";
+	if(isset($_POST["submitDelete"]) && $_POST["submitDelete"] == "DELETE"){
+		echo "<h1>ITEM DELETED</h1>";
+		$deleteUPC = $_POST['upc'];
+		echo $deleteUPC;
+		for($i=0;$i<count($basket);$i++){
+			$item = $basket[$i];
+			if($item['upc'] == $deleteUPC){
+				$basket[$i]=null;
+				break;
+			}
+		}
+	}
+	$_SESSION['shoppingBasket'] = $basket;;
+}
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+	//echo "<h1>POST</h1>";
+	if(isset($_POST["submitAdd"]) && $_POST["submitAdd"] == "ADD"){
+		echoBasket($basket);
+	}
+}
+else{
+	echoBasket($basket);
+}
+
+function echoBasket($basket){
+	echo "<form id=\"removeItem\" name=\"delete\" action=\"";
+	echo htmlspecialchars($_SERVER["PHP_SELF"]);
+	echo "\" method=\"POST\">";
+	// Hidden value is used if the delete link is clicked
+	echo "<input type=\"hidden\" name=\"upc\" value=\"-1\"/>";
+	// We need a submit value to detect if delete was pressed 
+	echo "<input type=\"hidden\" name=\"submitDelete\" value=\"DELETE\"/>";
+	echo "<h2>Shopping Basket</h2>";
+	echo "<table class='table' border=0 cellpadding =0 cellspacing=0>";
+	echo "<tr valine=center>";
+	$schema = array('upc','title','type','category','company','year','quantity','price');
+	for($i=0;$i<count($schema);$i++){
+		echo "<td class=rowheader>".$schema[$i]."</td>";
+	}
+	echo "</tr>";
+
+	for($i=0;$i<sizeof($basket);$i++){
+		echo $i;
+		$row = $basket[$i];
+		if($row == null){
+			echo"NULL";
+			continue;
+		}else{
+			for($j=0;$j<count($schema);$j++){
+				echo"<td>".$row[$schema[$j]]."</td>";
+			}
+		}
+		echo "<td>";
+		echo "<a href=\"javascript:formSubmit('".$row['upc']."');\">remove</a>";
+		echo"</td></tr>";
+	}
+	echo"</table>";
+	echo"<br><br>";
+	echo "</form>";
+}
+
+	?>				
 
 
 
-				echo "<form id=\"removeItem\" name=\"delete\" action=\"";
-				echo htmlspecialchars($_SERVER["PHP_SELF"]);
-				echo "\" method=\"POST\">";
-				// Hidden value is used if the delete link is clicked
-				echo "<input type=\"hidden\" name=\"upc\" value=\"-1\"/>";
-				// We need a submit value to detect if delete was pressed 
-				echo "<input type=\"hidden\" name=\"submitDelete\" value=\"DELETE\"/>";
-				echo "<h2>Shopping Basket</h2>";
-				echo "<table class='table' border=0 cellpadding =0 cellspacing=0>";
-				echo "<tr valine=center>";
-				$schema = array('upc','title','type','category','company','year','price','stock');
-				for($i=0;$i<count($schema);$i++){
-					echo "<td class=rowheader>".$schema[$i]."</td>";
-				}
-				echo "</tr>";
 
-				for($i=0;$i<sizeof($basket);$i++){
-					echo $i;
-					$row = $basket[$i];
-					if($row == null){
-						continue;
-					}else{
-						for($j=0;$j<count($schema);$j++){
-								echo"<td>".$row[$schema[$j]]."</td>";
-						}
-					}
-					echo "<td>";
-					echo "<a href=\"javascript:formSubmit('".$row['upc']."');\">remove</a>";
-					echo"</td></tr>";
-				}
-				echo"</table>";
-				echo"<br><br>";
-				echo "</form>";
-?>				
+
+
+
+
+
+
+
 
 				</div>
 				<div role="tabpanel" class="tab-pane active" id="shop">
 					<h3> Browsing Store Items </h3>
 <?php
-				//ITEM SELECTION TAB
-				
+//ITEM SELECTION TAB
 
-				echo "<form id=\"addItem\" name=\"add\" action=\"";
-				echo htmlspecialchars($_SERVER["PHP_SELF"]);
-				echo "\" method=\"POST\">";
-				// Hidden value is used if the delete link is clicked
-				echo "<input type=\"hidden\" name=\"upc\" value=\"-1\"/>";
-				// We need a submit value to detect if delete was pressed 
-				echo "<input type=\"hidden\" name=\"submitAdd\" value=\"ADD\"/>";
-				echo "<h2>Select Item</h2>";
-				echo "<table class='table' border=0 cellpadding =0 cellspacing=0>";
-				echo "<tr valine=center>";
 
-				$schema = array('upc','title','type','category','company','year','price','stock','quantity');
+echo "<form id=\"addItem\" name=\"add\" action=\"";
+echo htmlspecialchars($_SERVER["PHP_SELF"]);
+echo "\" method=\"POST\">";
+// Hidden value is used if the delete link is clicked
+echo "<input type=\"hidden\" name=\"upc\" value=\"-1\"/>";
+echo "<input type=\"hidden\" name=\"quantity\" value=\"-1\"/>";
+// We need a submit value to detect if delete was pressed 
+echo "<input type=\"hidden\" name=\"submitAdd\" value=\"ADD\"/>";
+echo "<h2>Select Item</h2>";
+echo "<table class='table' border=0 cellpadding =0 cellspacing=0>";
+echo "<tr valine=center>";
 
-				//SHOULD BE THE RESULT OF A SEARCH QUERY
-				$result = $P->getItems();
-				//NOT UST GETTING ITEMS
+$schema = array('upc','title','type','category','company','year','price','stock','quantity');
 
-				for($i=0;$i<count($schema);$i++){
-					echo "<td class=rowheader>".$schema[$i]."</td>";
-				}
-				echo "</tr>";
-				while($row = $result->fetch_assoc()){
-					for($j=0;$j<count($schema)-1;$j++){
-							echo"<td id=\"".$row[$schema[0]].$schema[$j]."\">".$row[$schema[$j]]."</td>";
-					}
-					echo "<td>";
-					echo "<input value=\"1\" size=\"2\"id=\"quantity".$row['upc']."\"></input>";
-					echo "<td>";
-					echo "<a href=\"javascript:addFormSubmit('".$row['upc']."');\">add</a>";
-					echo"</td></tr>";
-				}
-				echo"</table>";
-				echo"<br><br>";
+//SHOULD BE THE RESULT OF A SEARCH QUERY
+$result = $P->getItems();
+//NOT UST GETTING ITEMS
 
-				echo "</form>";
-				
-				if($_SERVER["REQUEST_METHOD"] == "POST") {
-					//echo "<h1>POST</h1>";
-					if(isset($_POST["submitAdd"]) && $_POST["submitAdd"] == "ADD"){
-						$addUPC = $_POST['upc'];
-						//echo $addUPC;
-						//echo "<h1>ITEM ADDED</h1>";
-						session_start();
-						
-						$addBasket = $_SESSION['shoppingBasket'];
-						
-						$result = $P->getItems();
-						while($row = $result->fetch_assoc()){
-							if($row['upc']==$addUPC){
-								$addItem = $row;
-								break;
-							}
-						}
-						echo $addItem['upc'];
-						//array_push($addBasket,$addItem);
-						$s = count($addBasket);
-						$addBasket[$s] = $addItem;
-						//echo $addBasket[0]['upc'];
+for($i=0;$i<count($schema);$i++){
+	echo "<td class=rowheader>".$schema[$i]."</td>";
+}
+echo "</tr>";
+while($row = $result->fetch_assoc()){
+	for($j=0;$j<count($schema)-1;$j++){
+		echo"<td id=\"".$row[$schema[0]].$schema[$j]."\">".$row[$schema[$j]]."</td>";
+	}
+	echo "<td>";
+	echo "<input value=\"1\" size=\"2\"id=\"quantity".$row['upc']."\"></input>";
+	echo "<td>";
+	echo "<a href=\"javascript:addFormSubmit('".$row['upc']."');\">add</a>";
+	echo"</td></tr>";
+}
+echo"</table>";
+echo"<br><br>";
 
-						$_SESSION['shoppingBasket'] = $addBasket;
-					}
-				}
+echo "</form>";
+
+
+/* Add an Item to The shopping basket */
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+	//echo "<h1>POST</h1>";
+	if(isset($_POST["submitAdd"]) && $_POST["submitAdd"] == "ADD"){
+		$addUPC = $_POST['upc'];
+		$quantity = $_POST['quantity'];
+		echo $quantity."<br>";
+		//echo $addUPC;
+		//echo "<h1>ITEM ADDED</h1>";
+		session_start();
+
+		$addBasket = $_SESSION['shoppingBasket'];
+
+		//get the Item based on the UPC
+		$result = $P->getItems();
+		while($row = $result->fetch_assoc()){
+			if($row['upc']==$addUPC){
+				$addItem = $row;
+				break;
+			}
+		}
+		echo $addItem['upc'];
+		/* Check if the Item is already in the basket and update it*/
+		for($i=0;$i<count($addBasket);$i++){
+			$row = $addBasket[$i];
+			if($row['upc'] == $addItem['upc']){
+				$row['quantity'] += $quantity;
+				$row['price'] += $quantity *$addItem['price'];
+				$addBasket[$i] = $row;
+				break;
+			}
+		}
+		$schema = array('upc','title','type','category','company','year','quantity','price');
+		echo $i."<br>";
+		/* This is a new item for the basket so it has to be added */
+		if($i == count($addBasket)){
+			$basketElement['upc']=$addItem['upc'];
+			$basketElement['title']=$addItem['title'];
+			$basketElement['type']=$addItem['type'];
+			$basketElement['category']=$addItem['category'];
+			$basketElement['year']=$addItem['year'];
+			$basketElement['quantity']= $quantity;
+			echo "inserting price";
+			$basketElement['price']= $quantity * $addItem['price'];
+			$s = count($addBasket);
+			$addBasket[$s] = $basketElement;
+		}
+
+		$_SESSION['shoppingBasket'] = $addBasket;
+		$_POST["submitAdd"] = null;
+	}
+}
+//Uncomment for ultra basket mode
+//echoBasket($addBasket);
 ?>
 				</div>
 			</div>
@@ -232,21 +269,21 @@ $P = new Presentation();
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-		
+
 <script>
 function formSubmit(upc) {
 	'use strict';
 	//document.write(upc);
-    if (confirm('Are you sure you want to delete this title?')) {
-      // Set the value of a hidden HTML element in this form
-      var form = document.getElementById('removeItem');
-      form.upc.value = upc;
-      // Post this form
-      form.submit();
-    }
+	if (confirm('Are you sure you want to delete this title?')) {
+		// Set the value of a hidden HTML element in this form
+		var form = document.getElementById('removeItem');
+		form.upc.value = upc;
+		// Post this form
+		form.submit();
+	}
 }
 </script>
-				
+
 <script>
 function addFormSubmit(upc) {
 	'use strict';
@@ -254,26 +291,26 @@ function addFormSubmit(upc) {
 	var title = document.getElementById(upc+'title').textContent;
 	var stock = document.getElementById(upc+'stock').textContent;
 	if(stock < quant){
-		if (confirm('You want '+quant+' but we only got '+stock+' so is '+stock+' ok?')) {/*
-	      // Set the value of a hidden HTML element in this form
-	      var form = document.getElementById('addItem');
-	      form.upc.value = upc;
-	      // Post this form
-	      form.submit();
+		if (confirm('You want '+quant+' but we only got '+stock+' so is '+stock+' ok?')) {
+			// Set the value of a hidden HTML element in this form
+			var form = document.getElementById('addItem');
+			form.upc.value = upc;
+			form.quantity.value = stock;
+			// Post this form
+			form.submit();
 		}
 		else{
-			return;
+			document.write("YOU SHOULD HAVE ACCEPTED");
 		}
-		 */
-			i	}
 	}
-	if (confirm('So you want to buy '+quant+' of '+title)) {
-      // Set the value of a hidden HTML element in this form
-      var form = document.getElementById('addItem');
-      form.upc.value = upc;
-      // Post this form
-      form.submit();
-    }
+	else if (confirm('So you want to buy '+quant+' of '+title)) {
+		// Set the value of a hidden HTML element in this form
+		var form = document.getElementById('addItem');
+		form.upc.value = upc;
+		form.quantity.value = quant;
+		// Post this form
+		form.submit();
+	}
 }
 </script>
 
